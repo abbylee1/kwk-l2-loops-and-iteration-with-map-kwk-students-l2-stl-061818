eef784d7dfecc224d25213181698d4cf6a8b645c
abbylee1 <ablee@micds.org>
2018-06-21 18:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 
-let studentArray = ['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
+//let studentArray = ['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
 
 function other(students) {
   return students.toLowerCase()
@@ -10,7 +10,11 @@ function lowerCaseStudentNames(names) {
   return names.map(other)
 }
 
-const fullNames = ['Amy Adams', 'Cate Blanchett', 'Emma Stone', 'Jennifer Lawrence', 'Natalie Portman']
+//const fullNames = ['Amy Adams', 'Cate Blanchett', 'Emma Stone', 'Jennifer Lawrence', 'Natalie Portman']
+
+function firstAndLast(actress) {
+  const actressFirst
+}
 
 function nameToAttributes(names) {
   return firstName()
